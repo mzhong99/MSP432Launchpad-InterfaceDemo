ee9e80df6bcf10214efa3fae65c5015b31f0bacc
Matthew Zhong <mzhong99@vt.edu>
2019-07-30 19:26:34 -0400

    Added missing project files

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LDS_SRCS += \
+../msp432p401r.lds 
+
+C_SRCS += \
+../Controller.c \
+../Demo.c \
+../LED.c \
+../Main.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Controller.d \
+./Demo.d \
+./LED.d \
+./Main.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Controller.o \
+./Demo.o \
+./LED.o \
+./Main.o \
+./system_msp432p401r.o 
+
+OBJS__QUOTED += \
+"Controller.o" \
+"Demo.o" \
+"LED.o" \
+"Main.o" \
+"system_msp432p401r.o" 
+
+C_DEPS__QUOTED += \
+"Controller.d" \
+"Demo.d" \
+"LED.d" \
+"Main.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../Controller.c" \
+"../Demo.c" \
+"../LED.c" \
+"../Main.c" \
+"../system_msp432p401r.c" 
+
+
---
+++ Debug/system_msp432p401r.d
@@ -0,0 +1,25 @@
+system_msp432p401r.o: ../system_msp432p401r.c \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp432p401r.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp_compatibility.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/core_cm4.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_compiler.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_gcc.h \
+ C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/core_cm4.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/third_party/CMSIS/Include/cmsis_gcc.h:
+
+C:/ti/simplelink_msp432p4_sdk_3_20_00_06/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
